a737ca68dd589a3c004e8a1a69390b30b51b37c3
yafiabiyyu <luckyveri3@gmail.com>
2018-10-21 01:13:55 +0700

    edit admin view

--- app/admin/views.py
+++ app/admin/views.py
@@ -4,14 +4,8 @@ from flask_login import login_required
 from . import admin
 
 
-@admin.route('/admin/pemiluraya/')
-def homepage():
-
-    return render_template('admin/index.html', title = "Welcome")
-
-
 
 @admin.route('/admin/dashboard/')
-def dashboard()
+def dashboard():
 
     return render_template('admin/dashboard.html', title = "Admin Dashboard")
